a025e3b80a3a9cdf51c53811ca3e6ddba7fb2f1d
Arman Peiravi <AMP3D@github.com>
2021-08-29 15:53:51 -0700

    Add JsonProperty to UserLoginMOdel

--- api/MovieBox/Models/UserLoginModel.cs
+++ api/MovieBox/Models/UserLoginModel.cs
@@ -1,14 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Newtonsoft.Json;
 
 namespace MovieBox.Models
 {
     public class UserLoginModel
     {
+        [JsonProperty("password")]
         public string Password { get; set; }
 
+        [JsonProperty("userName")]
         public string UserName { get; set; }
     }
 }
