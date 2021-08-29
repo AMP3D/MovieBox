b90f616c1078d73077eef2cf4df0eaa8e7f4a5fa
Arman Peiravi <AMP3D@github.com>
2021-08-29 15:49:54 -0700

    Create UserLoginModel.cs

---
+++ api/MovieBox/Models/UserLoginModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieBox.Models
+{
+    public class UserLoginModel
+    {
+        public string Password { get; set; }
+
+        public string UserName { get; set; }
+    }
+}
